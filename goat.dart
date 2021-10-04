42d174797692fdfd1f3464d3bba55bd7b87710bf
peterjidamva <64710848+peterjidamva@users.noreply.github.com>
2021-10-04 19:17:48 +0300

    Update and rename index.dart to goat.dart

---
+++ goat.dart
@@ -0,0 +1,36 @@
+//YOU CAN PERSONALIZE THIS FILE ! TO ADD IMAGE OF DIFFERENT ANIMAL FROM THE Goat/images/ directory
+
+
+import 'package:flutter/material.dart';
+import 'dart:math';
+class Goat extends StatelessWidget {
+  final direction ;
+  final goatSize ;
+  Goat(this.direction,this.goatSize);
+  @override
+  Widget build(BuildContext context) {
+
+    if(direction == "left"){
+        return Container(
+      width:goatSize,
+      height:goatSize,
+      child:Image.asset('goat/images/stand.png'),
+      // 
+    );
+    }
+
+    else {
+      return Transform(
+        alignment:Alignment.center,
+        transform:Matrix4.rotationY(pi),
+        child: Container(
+        width:goatSize,
+        height:goatSize,
+        child:Image.asset('goat/images/stand.png'),
+        // 
+          ),
+      );
+    }
+    
+  }
+}
